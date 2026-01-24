7c08ed6139c52ecec932759b5ce6a33239a4b3ab
stormozov <web@stormozov.ru>
2026-01-24 16:27:20 +0300

    refactor(cart):  доработать показ кол-ва товаров в виджете корзины
    Fixes #16

--- frontend/src/features/cart/ui/CartWidget/CartWidget.tsx
+++ frontend/src/features/cart/ui/CartWidget/CartWidget.tsx
@@ -1,5 +1,7 @@
 import classNames from "classnames";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
+
+import { useCart } from "@/features/cart";
 
 import "./CartWidget.scss";
 
@@ -7,22 +9,38 @@ import "./CartWidget.scss";
  * Интерфейс, описывающий свойства компонента {@link CartWidget}.
  */
 interface ICartWidgetProps {
+	/** Дополнительные классы */
 	classes?: string;
 }
 
 /**
  * Компонент виджета корзины.
- * 
+ *
  * Отображает иконку корзины и количество товаров в ней.
  */
 export function CartWidget({ classes }: ICartWidgetProps) {
+	const location = useLocation();
+	const {
+		states: { totalCount },
+	} = useCart();
+
+	const isCartPage = location.pathname === "/cart";
+	const hasItems = totalCount > 0;
+
 	return (
-		<div className="cart-widget">
-			<Link
-				to="/cart"
-				className={classNames("cart-widget__link", classes)}
-			></Link>
-			<div className="cart-widget__count">1</div>
+		<div className={classNames("cart-widget", classes)}>
+			{isCartPage ? (
+				<div className="cart-widget__icon" title="Вы уже на странице корзины" />
+			) : (
+				<Link
+					to="/cart"
+					className="cart-widget__link"
+					aria-label="Перейти в корзину"
+					title="Перейти в корзину"
+				/>
+			)}
+
+			{hasItems && <div className="cart-widget__count">{totalCount}</div>}
 		</div>
 	);
 }
