f3566ffa1512397b3357044b6be5ad3d272a4d38
stormozov <web@stormozov.ru>
2026-01-28 19:13:29 +0300

    feat(CI): доб. workflow для деплоя на GH Pages

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,52 +1,62 @@
 import { createBrowserRouter } from "react-router";
+
 import { lazyWithSuspense } from "./utils";
+
+const basename: string = import.meta.env.VITE_BASENAME;
 
 /**
  * Маршрутизация приложения.
  */
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: lazyWithSuspense(() => import("@/layouts/PageLayout/PageLayout")),
-		children: [
-			{
-				index: true,
-				element: lazyWithSuspense(() => import("@/pages/Homepage/Homepage")),
-			},
-			{
-				path: "/about",
-				element: lazyWithSuspense(() => import("@/pages/AboutPage/AboutPage")),
-			},
-			{
-				path: "/catalog",
-				element: lazyWithSuspense(
-					() => import("@/pages/CatalogPage/CatalogPage"),
-				),
-			},
-			{
-				path: "/catalog/:id",
-				element: lazyWithSuspense(
-					() => import("@/pages/ProductPage/ProductPage"),
-				),
-			},
-			{
-				path: "/contacts",
-				element: lazyWithSuspense(
-					() => import("@/pages/ContactsPage/ContactsPage"),
-				),
-			},
-			{
-				path: "/cart",
-				element: lazyWithSuspense(() => import("@/pages/CartPage/CartPage")),
-			},
-			{
-				path: "*",
-				element: lazyWithSuspense(
-					() => import("@/pages/PageNotFound/PageNotFound"),
-				),
-			},
-		],
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: lazyWithSuspense(
+				() => import("@/layouts/PageLayout/PageLayout"),
+			),
+			children: [
+				{
+					index: true,
+					element: lazyWithSuspense(() => import("@/pages/Homepage/Homepage")),
+				},
+				{
+					path: "/about",
+					element: lazyWithSuspense(
+						() => import("@/pages/AboutPage/AboutPage"),
+					),
+				},
+				{
+					path: "/catalog",
+					element: lazyWithSuspense(
+						() => import("@/pages/CatalogPage/CatalogPage"),
+					),
+				},
+				{
+					path: "/catalog/:id",
+					element: lazyWithSuspense(
+						() => import("@/pages/ProductPage/ProductPage"),
+					),
+				},
+				{
+					path: "/contacts",
+					element: lazyWithSuspense(
+						() => import("@/pages/ContactsPage/ContactsPage"),
+					),
+				},
+				{
+					path: "/cart",
+					element: lazyWithSuspense(() => import("@/pages/CartPage/CartPage")),
+				},
+				{
+					path: "*",
+					element: lazyWithSuspense(
+						() => import("@/pages/PageNotFound/PageNotFound"),
+					),
+				},
+			],
+		},
+	],
+	{ basename },
+);
 
 export default router;
--- frontend/src/shared/ui/HeaderLogo/HeaderLogo.tsx
+++ frontend/src/shared/ui/HeaderLogo/HeaderLogo.tsx
@@ -2,10 +2,12 @@ import { NavLink } from "react-router";
 
 import "./HeaderLogo.scss";
 
+const basename = import.meta.env.VITE_BASENAME;
+
 export default function HeaderLogo() {
 	return (
 		<NavLink to="/" className="header-logo">
-			<img src="/img/header/header-logo.png" alt="Bosa Noga" />
+			<img src={`${basename}img/header/header-logo.png`} alt="Bosa Noga" />
 		</NavLink>
 	);
 }
